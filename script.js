539d2ae5da785de47ec0f41218e307d6a7f6d2cb
Kayvon Kazemini <kayvonk@gmail.com>
2024-07-09 12:23:15 -0600

    updated fetch functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+let jokeHeader = document.getElementById("joke-header");
+let punchlineBtn = document.getElementById("punchline-btn");
+let newJoke = document.getElementById("new-joke");
+let punchline;
+
+function fetchJoke() {
+  const queryUrl = "https://manateejokesapi.herokuapp.com/manatees/random";
+
+  fetch(queryUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      jokeHeader.textContent = data.setup;
+      punchline = data.punchline;
+    });
+}
+
+fetchJoke();
+
+function showPunchline() {
+  jokeHeader.textContent = punchline;
+  punchlineBtn.style.display = "none";
+  newJoke.style.display = "block";
+}
+
+punchlineBtn.addEventListener("click", showPunchline);
+newJoke.addEventListener("click", function () {
+  location.reload();
+});
